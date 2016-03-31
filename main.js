d7b83cdbfd3b7e3aa60f78c17c8bdd02d479905b
Sasha Semanyuk <s.semanyuk@gmail.com>
2016-03-31 21:52:04 +0300

    add sort navigation

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	var currentData;
+	var pokemonTypeLabel = ["water", "grass", "poison", "ground", "fire", "electric", "bug", "flying", "fairy", "fighting", "physics", "rock", "steel", "ice", "ghost", "dragon", "dark"];
+	var pokemonTypeColor = ["#25d5f2", "#21fbb0", "#b514aa", "brown", "#f04877", "#dfcf8f", "#949494", "#ffa500", "#cfd05c", "#547ded", "#29b7ce", "#a19898", "#b8d1e7", "#00adef", "rgba(255,255,255, .9)", "#ff6666", 
+"#ba8d38", "#eae5e5"];
 	var x = $.ajax({
 		type: "GET",
 		url: "http://pokeapi.co/api/v1/pokemon/?limit=12",
@@ -7,6 +10,7 @@ $(document).ready(function(){
 		beforeSend: function() {
 			$('.main_header').hide();
 			$('.next_page').hide();
+			
 		},
 		cache: true,
 		success: loadData
@@ -15,21 +19,21 @@ $(document).ready(function(){
 	function loadData(data) {
 		appendData(data);
 		$('.main_header').show();
+		$('.navigation').show();
 		$('.next_page').css("display", "flex");
 	}
 	//function for taking data from server and make pokemon_card
 	function appendData(data){
 		currentData = data;
-
-		console.log(currentData)
 		$.each(currentData.objects, function(index, element) {
 			var source = $("#pokemon-template").html();
 			var template = Handlebars.compile($.trim(source));
 			var output = template(element);
 			$('.pokemon_list').append(output);
-			for(var i = 0; i<$('.pokemon_list-skills-item').length; i++) { 
-			  labels(i);
-			}
+		});
+
+		$('.pokemon_list-skills-item').each(function(){
+			$(this).css('background', pokemonTypeColor[pokemonTypeLabel.indexOf($(this).html())]);
 		});
 	};
 
@@ -78,105 +82,19 @@ $(document).ready(function(){
 		$.get("http://pokeapi.co" + currentData.meta.next, loadData);
 	});
 
-	//taking the color for the label at card
-	function labels(i) { 
-		switch($('.pokemon_list-skills-item')[i].innerHTML) {
-			case "water":
-				$('.pokemon_list-skills-item')[i].style.background = "#25d5f2";
-				break;
-			case "grass": 
-				$('.pokemon_list-skills-item')[i].style.background = "#21fbb0";
-				break;
-			case "poison":
-				$('.pokemon_list-skills-item')[i].style.background = "#b514aa";
-				break;
-			case "ground":
-				$('.pokemon_list-skills-item')[i].style.background = "brown";
-				break;
-			case "fire":
-				$('.pokemon_list-skills-item')[i].style.background = "#f04877";
-				break;
-			case "electric":
-				$('.pokemon_list-skills-item')[i].style.background = "#dfcf8f";
-				break;
-			case "bug": 
-				$('.pokemon_list-skills-item')[i].style.background = "#949494";
-				break;
-			case "flying":
-				$('.pokemon_list-skills-item')[i].style.background = "#ffa500";
-				break;
-			case "fairy":
-				$('.pokemon_list-skills-item')[i].style.background = "#cfd05c";
-				break;
-			case "fighting":
-				$('.pokemon_list-skills-item')[i].style.background = "#e5447ded";
-				break;
-			case "physics":
-				$('.pokemon_list-skills-item')[i].style.background = "#29b7ce";
-				break;
-			case "rock":
-				$('.pokemon_list-skills-item')[i].style.background = "#a19898";
-				break;
-			case "steel":
-				$('.pokemon_list-skills-item')[i].style.background = "#b8d1e7";
-				break;
-			case "ice":
-				$('.pokemon_list-skills-item')[i].style.background = "#00adef";
-				break;
-			case "ghost":
-				$('.pokemon_list-skills-item')[i].style.background = "rgba(255,255,255, .9)";
-				break;
-			case "dragon":
-				$('.pokemon_list-skills-item')[i].style.background = "#ff6666";
-				break;
-			case "dark":
-				$('.pokemon_list-skills-item')[i].style.background = "#ba8d38";
-				break;
-			default:
-				$('.pokemon_list-skills-item')[i].style.background = "#eae5e5;"
-		}	
-	}
-
 	$(".sort").on('click', function(){
-	/*	var ul = $(".pokemon_list");
-		var arr = $.makeArray(ul.children("li"));
+		var $allListElements = $('.pokemon_list > li');
+	    var $matchingListElements = $allListElements.filter(function(i, li){
+	        var listItemText = $(li).text().toLowerCase(), 
+	            searchText = $('select option:selected').text().toLowerCase();
+	        return ~listItemText.indexOf(searchText);
+	    });
 
-		arr.sort(function(a, b) {
-		    var textA = $(a).data("type");
-		    var textB = $(b).data("type");
-		    
-			
-			
-		});
-
-		ul.empty();
-
-		$.each(arr, function() {
-		    ul.append(this);
-		});
-	})*/
-
-
-    var that = this, 
-    	$allListElements = $('.pokemon_list > li');
-
-    var $matchingListElements = $allListElements.filter(function(i, li){
-        var listItemText = $(li).text().toLowerCase(), 
-            searchText = "normal".toLowerCase();
-        return ~listItemText.indexOf(searchText);
-    });
-
-    $allListElements.hide();
-    $matchingListElements.show();
-
-
-
-
+	    $allListElements.hide();
+	    $matchingListElements.show();
 	});
-
-	 $('body').on('click', '.reset', function() {
+	
+	$('body').on('click', '.reset', function() {
 		$('.pokemon_list > li').show();
 	})
-
 });
-
